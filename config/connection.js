dcc22fd643a7afe9e853c4d173ccd56fa9a19c38
Madison Kalivoda <mkalivoda07@gmail.com>
2018-03-01 19:12:34 -0700

    fix handlebars main file & deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -29,15 +29,15 @@ module.exports = connection;
 var mysql = require('mysql');
 var connection;
 
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'Codem07!',
-        database: 'burgers_db'
+    host: 'localhost',
+    user: 'root',
+    password: 'Codem07!',
+    database: 'burgers_db'
     });
 };
 
